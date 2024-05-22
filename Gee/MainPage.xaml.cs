890c8ab70777df12e310e34c9eb2939abf8a123f
Ian Bravyn. <ianbravynsa@gmail.com>
2024-05-22 14:26:13 +0300

    Add project files.

---
+++ Gee/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace Gee
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                Main.Text = $"Hello {count} time";
+            else
+                Main.Text = $"Hello {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
---
+++ Gee/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Gee
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
